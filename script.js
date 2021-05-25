0c887a5bd09853b4ef233cee3d67cfd3012565b7
Ciarm-nik <nikcia@libero.it>
2021-05-25 15:19:47 +0200

    Prime modifiche vuejs

---
+++ script.js
@@ -0,0 +1,30 @@
+new Vue({
+    el:"#app",
+    data: {
+        navbarLinks: [{
+            text: "Home",
+            link:"#"
+        },
+        {
+            text: "Services",
+            link:"#"
+        },
+        {
+            text: "Why Us",
+            link:"#"
+        },
+        {
+            text: "Case Studies",
+            link:"#"
+        },
+        {
+            text: "About",
+            link:"#"
+        },
+        {
+            text: "Blog",
+            link:"#"
+        }]
+    }
+
+})
