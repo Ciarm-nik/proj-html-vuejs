7f52d748f591ae8b8e1fb99c9184437db02b2950
Ciarm-nik <nikcia@libero.it>
2021-05-25 19:26:16 +0200

    Nav e footer con vuejs competi

--- script.js
+++ script.js
@@ -1,30 +1,114 @@
 new Vue({
-    el:"#app",
+    el: "#app",
     data: {
         navbarLinks: [{
+            // Dropdown menu Home
             text: "Home",
-            link:"#"
+            link: "#",
+            childs: [
+                {
+                    text: "1",
+                    link: "#",
+                },
+                {
+                    text: "2",
+                    link: "#",
+                },
+                {
+                    text: "3",
+                    link: "#",
+                },
+            ]
         },
         {
+            // Dropdown menu Services
             text: "Services",
-            link:"#"
+            link: "#"
         },
         {
             text: "Why Us",
-            link:"#"
+            link: "#"
         },
         {
             text: "Case Studies",
-            link:"#"
+            link: "#"
         },
         {
             text: "About",
-            link:"#"
+            link: "#"
         },
         {
             text: "Blog",
-            link:"#"
-        }]
+            link: "#"
+        },
+        // Icon + numero(text)
+        {
+            icon: "fa-phone",
+            text: "(555) 802-1234",
+            link: ""
+        },
+        // Button
+        {
+            // text: "Free Quote",
+            // link: "#",
+            // button: "button"
+        }],
+        footerLinks: {
+            services: [{
+                text: "Marketing Plan",
+                link: "#"
+            },
+            {
+                text: "Sales Development",
+                link: "#"
+            },
+            {
+                text: "Digital Marketing",
+                link: "#"
+            },
+            {
+                text: "Pricing",
+                link: "#"
+            },
+            {
+                text: "Why Us",
+                link: "#"
+            },
+            {
+                text: "Case Studies",
+                link: "#"
+            },
+            ],
+            resources: [{
+                text: "Learning Center",
+                link: "#"
+            },
+            {
+                text: "Video Tutorials",
+                link: "#"
+            },
+            {
+                text: "Customers",
+                link: "#"
+            },
+            {
+                text: "Blog",
+                link: "#"
+            },
+            ],
+            company: [{
+                text: "Who We Are",
+                link: "#"
+            },
+            {
+                text: "Contact Us",
+                link: "#"
+            },
+            {
+                text: "Careers",
+                link: "#"
+            },
+            ],
+        }
     }
-
 })
